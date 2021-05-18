470f4dadd11efb3b43d251e8da7400268aedf53e
ebouther <ebouther@student.42.fr>
2021-05-18 17:18:14 +0200

    feat: octokit - commit->push changes

--- packages/content/lib/middleware.js
+++ packages/content/lib/middleware.js
@@ -1,7 +1,106 @@
+const { Octokit, App, Action } = require("@octokit/rest");
+const { uuid } = require('uuidv4');
+const { relative } = require('path');
+
+const octokit = new Octokit({ auth: process.env.TOKEN });
+
 const { join } = require('path')
 const fs = require('graceful-fs').promises
 const nodeReq = require('node-req')
 const nodeRes = require('node-res')
+
+
+/* Github update of @filePath with @content */
+const octoUpdate = async ({filePath, content}) => {
+
+  const owner = 'ebouther';
+  const repo = 'nuxt-content-github'
+  const branch = 'master';
+
+
+  const { data: { object: { sha: currCommitSha }} } = await octokit.git.getRef({
+    owner,
+    repo,
+    ref: `heads/${branch}`,
+  });
+  console.log('commit sha: ', currCommitSha);
+
+
+  const { data: { tree: { sha: treeSha } } } = await octokit.git.getCommit({
+    owner,
+    repo,
+    commit_sha: currCommitSha,
+  })
+  console.log('tree sha: ', treeSha);
+
+
+  const newBranch = `heads/octo/${uuid()}`;
+
+  /* Create Branch */
+  const ref = await octokit.rest.git.createRef({
+    owner,
+    repo,
+    ref: `refs/${newBranch}`,
+    sha: currCommitSha,
+  });
+
+
+  /* Create Blob */
+  //const content = "OCTO FILE!!!"; // await getFileAsUTF8(filePath)
+  const { data: { sha: blobSha, url } } = await octokit.git.createBlob({
+    owner,
+    repo,
+    content,
+    encoding: 'utf-8',
+  });
+  console.log('BLOB SHA: ', blobSha, url);
+
+  /* Create Tree */
+  const tree = [{sha: blobSha, path: filePath}].map(({ sha, path }, index) => ({
+    path,
+    mode: `100644`,
+    type: `blob`,
+    sha: blobSha,
+  }));
+  console.log(tree);
+
+  const { data: { sha: newTreeSha } } = await octokit.git.createTree({
+    owner,
+    repo,
+    tree,
+    base_tree: treeSha,
+  });
+  console.log('newTree: ', newTreeSha);
+
+
+  /* Create Commit */
+  const { data: newCommit } = await octokit.git.createCommit({
+    owner,
+    repo,
+    message: 'Octo Commit !',
+    tree: newTreeSha,
+    parents: [currCommitSha],
+  });
+
+  console.log('NEW COMMIT :', newCommit);
+
+  /* Update Branch */
+  const updateRef = await octokit.git.updateRef({
+    owner,
+    repo,
+    ref: newBranch,
+    sha: newCommit.sha,
+  })
+
+  console.log('UPDATE REF :', updateRef);
+
+
+
+};
+
+
+
+
 
 module.exports = ({ ws, database, dir, watch }) => async (req, res) => {
   const url = decodeURI(nodeReq.url(req))
@@ -44,6 +143,12 @@ module.exports = ({ ws, database, dir, watch }) => async (req, res) => {
     const filePath = join(dir, url)
     const stats = await fs.lstat(filePath).catch(_ => null)
     if (req.method === 'PUT' && stats && stats.isFile()) {
+
+      // ==============================
+      await octoUpdate({filePath: relative(process.cwd(), filePath), content: params.file});
+      console.log("FILE UPDATED : ", filePath);
+      // ==============================
+
       await fs.writeFile(filePath, params.file, 'utf-8')
 
       return nodeRes.send(req, res, {})
