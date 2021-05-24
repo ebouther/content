477250ac88fa1590a9ec2ca6e85e0c6f88f29ad7
ebouther <ebouther@student.42.fr>
2021-05-24 17:31:23 +0200

    feat: merge edit changes to main branch

--- packages/content/templates/github.js
+++ packages/content/templates/github.js
@@ -11,13 +11,22 @@ const octokit = new Octokit({
   auth: {
     appId: '<%= options.appId %>',
     privateKey: `<%= options.privateKey %>`,
-    installationId: '<%= options.installationId %>',
+    installationId: '<%= options.installationId %>'
   },
 });
 
+console.log(octokit)
+
 
 const fetchFile = async (filePath) => {
-  return 'FAKE FILE CONTENT';
+
+  const { data: { content, encoding } } = await octokit.rest.repos.getContent({
+    owner,
+    repo,
+    path: filePath,
+    ref: `heads/${branch}`,
+  });
+  return (new Buffer(content, encoding)).toString('utf-8');
 }
 
 /* Github update of @filePath with @content */
@@ -41,13 +50,13 @@ const saveFile = async ({filePath, content}) => {
   console.log('tree sha: ', treeSha);
 
 
-  const newBranch = `heads/octo/${uuid()}`;
+  const newBranch = `content/${uuid()}`;
 
   /* Create Branch */
   const ref = await octokit.rest.git.createRef({
     owner,
     repo,
-    ref: `refs/${newBranch}`,
+    ref: `refs/heads/${newBranch}`,
     sha: currCommitSha,
   });
 
@@ -95,11 +104,56 @@ const saveFile = async ({filePath, content}) => {
   const updateRef = await octokit.git.updateRef({
     owner,
     repo,
-    ref: newBranch,
+    ref: `heads/${newBranch}`,
     sha: newCommit.sha,
   })
 
   console.log('UPDATE REF :', updateRef);
+
+  /* Create Pull Request */
+  // const pr = await octokit.rest.pulls.create({
+  //   title: filePath + ' update',
+  //   owner,
+  //   repo,
+  //   head: newBranch,
+  //   base: branch,
+  // });
+
+  // console.log('PULL REQUEST : ', pr);
+
+  // // Wait for mergeable state to be known
+  // if (pr.mergeable_state === "unknown") {
+  //   
+  // }
+
+  // if (!pr.mergeable) {
+  //   console.log('Merge conflict - Please merge the Pull Request manually.')
+  //   return;
+  // }
+
+  // const merge = await octokit.rest.pulls.merge({
+  //   owner,
+  //   repo,
+  //   pull_number: pr.number,
+  // });
+
+  // console.log('PR Merged : ', merge);
+
+
+  const merge = await octokit.rest.repos.merge({
+    owner,
+    repo,
+    head: newBranch,
+    base: branch,
+  });
+
+  if (merge.status === 409) {
+    console.log('Merge conflict')
+  }
+
+  console.log ('MERGE : ', merge);
+
+  // TODO: deletePR && deleteRef
 };
 
 export default {
